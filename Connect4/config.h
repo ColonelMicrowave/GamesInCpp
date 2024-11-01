5e9f7d7aeb9aec277fbfc5d76c550913d7e37707
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-11-01 12:27:57 +0000

    Create config.h

---
+++ Connect4/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+namespace Config
+{
+	const int ROWS{ 6 };
+	const int COLS{ 7 };
+	const char EMPTY{ '.' };
+	const char PLAYER1{ 'X' };
+	const char PLAYER2{ 'O' };
+}
+
+#endif
