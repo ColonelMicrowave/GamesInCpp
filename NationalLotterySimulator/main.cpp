d2bb17586e620b0680c5ee5c644d75e46e4e9f12
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-11-15 17:22:00 +0000

    Improved accuracy of the simulation

--- NationalLotterySimulator/main.cpp
+++ NationalLotterySimulator/main.cpp
@@ -15,87 +15,104 @@ int generateBonusBall()
 	return Random::get(1, 7);
 }
 
+long long calculateMean(const std::vector<int>& numbers)
+{
+	long long sum{ 0 };
+	for (int number : numbers)
+	{
+		sum += number;
+	}
+	return sum / numbers.size();
+}
+
 void simulateLottery()
 {
-	int six{ 0 };
-	int fiveBonus{ 0 };
-	int five{ 0 };
-	int four{ 0 };
-	int three{ 0 };
-	int two{ 0 };
-	int one{ 0 };
-	int zero{ 0 };
+	constexpr int sims{ 5 }; // Adjust this value to increase or decrease the number of simulations
+	constexpr int initialSize{ 100'000'000 }; // Adjust this value to increase or decrease the number of tickets bought per simulation
 
-	for (int i{ 0 }; i < 100'000'000; i++)
+	std::vector<int> six(sims);
+	std::vector<int> fiveBonus(sims);
+	std::vector<int> five(sims);
+	std::vector<int> four(sims);
+	std::vector<int> three(sims);
+	std::vector<int> two(sims);
+	std::vector<int> one(sims);
+	std::vector<int> zero(sims);
+
+	for (int i{ 0 }; i < sims; ++i)
 	{
-		std::vector<int> winningNumbers(6);
-		std::vector<int> playerNumbers(6);
 
-		int bonusBall{ generateBonusBall() };
-		int playerBonusBall{ generateBonusBall() };
-
-		generateLotteryNumbers(winningNumbers);
-		generateLotteryNumbers(playerNumbers);
-
-		int correctNumbers{ 0 };
-		std::vector<int> tempWinningNumbers{ winningNumbers };
-		for (int j : playerNumbers)
+		for (int j{ 0 }; j < initialSize; ++j)
 		{
-			for (int& k : tempWinningNumbers)
+			std::vector<int> winningNumbers(6, 0);
+			std::vector<int> playerNumbers(6, 0);
+
+			int bonusBall{ generateBonusBall() };
+			int playerBonusBall{ generateBonusBall() };
+
+			generateLotteryNumbers(winningNumbers);
+			generateLotteryNumbers(playerNumbers);
+
+			int correctNumbers{ 0 };
+			std::vector<int> tempWinningNumbers{ winningNumbers };
+			for (int guess : playerNumbers)
 			{
-				if (j == k)
+				for (int& actual : tempWinningNumbers)
 				{
-					++correctNumbers;
-					k = -1;
-					break;
+					if (guess == actual)
+					{
+						++correctNumbers;
+						actual = -1;
+						break;
+					}
 				}
 			}
-		}
 
-		switch (correctNumbers)
-		{
-		case 6:
-			++six;
-			break;
-		case 5:
-			if (playerBonusBall == bonusBall)
+			switch (correctNumbers)
 			{
-				++fiveBonus;
+			case 6:
+				++six[i];
+				break;
+			case 5:
+				if (playerBonusBall == bonusBall)
+				{
+					++fiveBonus[i];
+				}
+				else
+				{
+					++five[i];
+				}
+				break;
+			case 4:
+				++four[i];
+				break;
+			case 3:
+				++three[i];
+				break;
+			case 2:
+				++two[i];
+				break;
+			case 1:
+				++one[i];
+				break;
+			case 0:
+				++zero[i];
+				break;
+			default:
+				std::cout << "Error: Invalid number of correct numbers\n";
+				break;
 			}
-			else
-			{
-				++five;
-			}
-			break;
-		case 4:
-			++four;
-			break;
-		case 3:
-			++three;
-			break;
-		case 2:
-			++two;
-			break;
-		case 1:
-			++one;
-			break;
-		case 0:
-			++zero;
-			break;
-		default:
-			std::cout << "Error: Invalid number of correct numbers\n";
-			break;
 		}
 	}
 
-	std::cout << "Jackpot (6 numbers): " << six << '\n';
-	std::cout << "5 numbers + bonus: " << fiveBonus << '\n';
-	std::cout << "5 numbers: " << five << '\n';
-	std::cout << "4 numbers: " << four << '\n';
-	std::cout << "3 numbers: " << three << '\n';
-	std::cout << "2 numbers: " << two << '\n';
-	std::cout << "1 number: " << one << '\n';
-	std::cout << "0 numbers: " << zero << '\n';
+	std::cout << "Probability of winning the National Lottery:\n";
+	std::cout << "Six numbers: " << calculateMean(six) << " in " << initialSize <<'\n';
+	std::cout << "Five numbers and the bonus ball: " << calculateMean(fiveBonus) << " in " << initialSize << '\n';;
+	std::cout << "Five numbers: " << calculateMean(five) << " in " << initialSize << '\n';;
+	std::cout << "Four numbers: " << calculateMean(four) << " in " << initialSize << '\n';;
+	std::cout << "Three numbers: " << calculateMean(three) << " in " << initialSize << '\n';;
+	std::cout << "Two numbers: " << calculateMean(two) << " in " << initialSize << '\n';;
+	std::cout << "One number: " << calculateMean(one) << " in " << initialSize << '\n';;
 }
 
 int main()
