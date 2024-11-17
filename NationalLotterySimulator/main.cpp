0a2e72a15cdc9aab780c0369dc6fe77ea02e5e8a
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-11-17 21:33:21 +0000

    Player can now play the lottery
    Lottery now uses the real rules

--- NationalLotterySimulator/main.cpp
+++ NationalLotterySimulator/main.cpp
@@ -1,33 +1,134 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 #include "random.h"
 
+void ignoreLine()
+{
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void setColour(int colour)
+{
+	std::cout << "\033[" << colour << "m";
+}
+
+void resetColour()
+{
+	std::cout << "\033[0m";
+}
+
 void generateLotteryNumbers(std::vector<int>& lotteryNumbers)
 {
-	for (int i{ 0 }; i < 6; ++i)
+	for (int i = 0; i < 6; ++i)
 	{
-		lotteryNumbers.at(i) = Random::get(1, 59);
+		int newNumber = Random::get(1, 59);
+
+		// Check if the number is already in the vector
+		while (std::find(lotteryNumbers.begin(), lotteryNumbers.end(), newNumber) != lotteryNumbers.end())
+		{
+			newNumber = Random::get(1, 59);  // Generate a new number if duplicate found
+		}
+
+		lotteryNumbers.at(i) = newNumber;
 	}
 }
 
-int generateBonusBall()
+int generateBonusBall(const std::vector<int>& numbers)
 {
-	return Random::get(1, 7);
+	while (true)
+	{
+		bool isInUse{ false };
+		int bonus{ Random::get(1, 59) };
+		for (int i : numbers)
+		{
+			if (i == bonus)
+			{
+				isInUse = true;
+			}
+		}
+
+		if (!isInUse)
+			return bonus;
+	}
 }
 
-long long calculateMean(const std::vector<int>& numbers)
+int calculateMean(const std::vector<int>& numbers)
 {
-	long long sum{ 0 };
+	int sum{ 0 };
 	for (int number : numbers)
 	{
 		sum += number;
 	}
-	return sum / numbers.size();
+	return sum / std::ssize(numbers);
+}
+
+int getPlayerNumber(const std::vector<int>& previousNumbers)
+{
+	while (true)
+	{
+		std::cout << "Enter your number (1-59): ";
+		int userNum{};
+		std::cin >> userNum;
+
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			ignoreLine();
+			std::cout << "Invalid input. Try again.\n";
+			continue;
+		}
+
+		ignoreLine();
+
+		if (userNum > 59 || userNum < 1)
+		{
+			std::cout << "Number out of bounds. Try again.\n";
+			continue;
+		}
+
+		bool isDuplicate = false;
+		for (int num : previousNumbers)
+		{
+			if (num != 0 && num == userNum)
+			{
+				isDuplicate = true;
+				break;
+			}
+		}
+
+		if (isDuplicate)
+		{
+			std::cout << "Duplicate number. Try again.\n";
+			continue;
+		}
+
+		return userNum;
+	}
+}
+
+int checkForCorrectNumber(const std::vector<int>& playerNumbers, std::vector<int> winningNumbers)
+{
+	int correctNumbers{ 0 };
+	for (int guess : playerNumbers)
+	{
+		for (int& actual : winningNumbers)
+		{
+			if (guess == actual)
+			{
+				++correctNumbers;
+				actual = -1;
+				break;
+			}
+		}
+	}
+
+	return correctNumbers;
 }
 
 void simulateLottery()
 {
-	constexpr int sims{ 5 }; // Adjust this value to increase or decrease the number of simulations
+	constexpr int sims{ 1 }; // Adjust this value to increase or decrease the number of simulations
 	constexpr int initialSize{ 100'000'000 }; // Adjust this value to increase or decrease the number of tickets bought per simulation
 
 	std::vector<int> six(sims);
@@ -47,26 +148,13 @@ void simulateLottery()
 			std::vector<int> winningNumbers(6, 0);
 			std::vector<int> playerNumbers(6, 0);
 
-			int bonusBall{ generateBonusBall() };
-			int playerBonusBall{ generateBonusBall() };
-
 			generateLotteryNumbers(winningNumbers);
 			generateLotteryNumbers(playerNumbers);
 
-			int correctNumbers{ 0 };
-			std::vector<int> tempWinningNumbers{ winningNumbers };
-			for (int guess : playerNumbers)
-			{
-				for (int& actual : tempWinningNumbers)
-				{
-					if (guess == actual)
-					{
-						++correctNumbers;
-						actual = -1;
-						break;
-					}
-				}
-			}
+			int bonusBall{ generateBonusBall(winningNumbers) };
+
+			int correctNumbers{ checkForCorrectNumber(playerNumbers, winningNumbers) };
+			bool isBonus{ false };
 
 			switch (correctNumbers)
 			{
@@ -74,14 +162,20 @@ void simulateLottery()
 				++six[i];
 				break;
 			case 5:
-				if (playerBonusBall == bonusBall)
+				for (int guess : playerNumbers)
 				{
+					if (guess == bonusBall)
+					{
+						isBonus = true;
+						break;
+					}
+				}
+
+				if (isBonus)
 					++fiveBonus[i];
-				}
 				else
-				{
 					++five[i];
-				}
+
 				break;
 			case 4:
 				++four[i];
@@ -115,11 +209,140 @@ void simulateLottery()
 	std::cout << "One number: " << calculateMean(one) << " in " << initialSize << '\n';;
 }
 
+void playLottery()
+{
+	std::vector<int> winningNumbers(6);
+	std::vector<int> playerNumbers(6);
+
+	generateLotteryNumbers(winningNumbers);
+
+	for (int& i : playerNumbers)
+	{
+		i = getPlayerNumber(playerNumbers);
+	}
+
+	int bonusBall{ generateBonusBall(winningNumbers) };
+
+	int correctNumbers{ checkForCorrectNumber(playerNumbers, winningNumbers) };
+	bool isBonus{ false };
+
+	std::cout << "Winning Numbers: ";
+	for (int number : winningNumbers)
+	{
+		std::cout << number << ' ';
+	}
+	std::cout << "\nBonus Ball: " << bonusBall << "\n";
+
+	std::cout << "Your Numbers: ";
+	for (int playerNumber : playerNumbers)
+	{
+		bool isMatch{ false };
+		for (int winningNumber : winningNumbers)
+		{
+			if (playerNumber == winningNumber)
+			{
+				isMatch = true;
+				break;
+			}
+		}
+
+		if (isMatch)
+		{
+			setColour(32);
+			std::cout << playerNumber << ' ';
+			resetColour();
+		}
+		else
+		{
+			setColour(34);
+			std::cout << playerNumber << ' ';
+			resetColour();
+		}
+	}
+	std::cout << '\n';
+
+	switch (correctNumbers)
+	{
+	case 6:
+		std::cout << "You won the jackpot prize: \x9C 3,000,000!\n";
+		break;
+
+	case 5:
+		for (int guess : playerNumbers)
+		{
+			if (guess == bonusBall)
+			{
+				isBonus = true;
+				break;
+			}
+		}
+
+		if (isBonus)
+			std::cout << "You won the bonus ball + 5 numbers! You earned \x9C 1,000,000!\n";
+		else
+			std::cout << "You won 5 numbers! You earned \x9C 1,750!\n";
+
+		break;
+
+	case 4:
+		std::cout << "You won 4 numbers! You earned \x9C 140!\n";
+		break;
+
+	case 3:
+		std::cout << "You won 3 numbers! You earned \x9C 30!\n";
+		break;
+
+	case 2:
+		std::cout << "You won 2 numbers! You earned a free Lucky Dip!\n";
+		break;
+
+	case 1: [[fallthrough]];
+	case 0:
+		std::cout << "Unfortunately, you did not win anything. Better luck next time!\n";
+		break;
+
+	default:
+		std::cout << "What have you done!\n";
+		break;
+	}
+}
+
+char playerChoice()
+{
+	while (true)
+	{
+		std::cout << "Would you like to play (p) or simulate (s) the National Lottery Lotto: ";
+		char choice{};
+		std::cin >> choice;
+
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			ignoreLine();
+			std::cout << "Invalid input. Try again!\n";
+
+			continue;
+		}
+
+		ignoreLine();
+
+		if (choice == 'p' || choice == 'P')
+			return 'p';
+		else if (choice == 's' || choice == 'S')
+			return 's';
+		else
+			std::cout << "Invalid input. Try again!\n";
+	}
+}
+
 int main()
 {
 	std::cout << "Welcome to the National Lottery Simulator!\n";
 
-	simulateLottery();
+	if (playerChoice() == 'p')
+		playLottery();
+	else
+		simulateLottery();
 
 	return 0;
 }
